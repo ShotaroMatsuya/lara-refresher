2942b51472f8c82c01645ed10d87fac4e9aeb863
ShotaroMatsuya <matsuya@h01.itscom.net>
2020-11-03 14:26:04 +0900

    fixed routing in notification-page

--- DDDDD/resources/views/users/notifications.blade.php
+++ DDDDD/resources/views/users/notifications.blade.php
@@ -13,11 +13,11 @@
                 @if ($notification->type== 'LaravelForum\Notifications\NewReplyAdded')
                 あなたの質問に対して回答が新しく追加されました
                 <strong>{{$notification->data['discussion']['title']}}</strong>
-            <a href="{{route('discussions.show',$notification->data['discussion']['slug'])}}" class="btn float-right btn-sm btn-info">View discussion</a>
+            <a href="{{route('discussions.show',$notification->data['discussion']['id'])}}" class="btn float-right btn-sm btn-info">View discussion</a>
                 @endif
                 @if ($notification->type== 'LaravelForum\Notifications\ReplyMarkedAsBestReply')
                     あなたの回答が「 <strong>{{$notification->data['discussion']['title']}}</strong> 」にてベストアンサーに認定されました！！
-                    <a href="{{route('discussions.show',$notification->data['discussion']['slug'])}}" class="btn float-right btn-sm btn-info">確認する</a>
+                    <a href="{{route('discussions.show',$notification->data['discussion']['id'])}}" class="btn float-right btn-sm btn-info">確認する</a>
 
                 @endif
 
