41af38ff88905e25f6f4159a84baa66ea514c8c3
ShotaroMatsuya <matsuya@h01.itscom.net>
2020-11-03 13:44:02 +0900

    fixed start_artisan.sh

--- DDDDD/start_artisan.sh
+++ DDDDD/start_artisan.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-/usr/local/php/7.3/bin/php /home/users/2/candypop.jp-wispy-moji-9349/web/lara-forum/artisan schedule:run >> /dev/null 2>&1
+cd /home/users/2/candypop.jp-wispy-moji-9349/web/lara-forum/
+/usr/local/php/7.3/bin/php artisan cache:clear
+/usr/local/php/7.3/bin/php artisan schedule:run >> /dev/null 2>&1
