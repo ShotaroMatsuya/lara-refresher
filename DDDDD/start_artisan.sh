ad2a7ce498e22c9292dc0f7a207827bd0dac0b35
ShotaroMatsuya <matsuya@h01.itscom.net>
2020-11-03 13:29:48 +0900

    fixed cron task-scheduler

--- DDDDD/start_artisan.sh
+++ DDDDD/start_artisan.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/local/php/7.3/bin/php /home/users/2/candypop.jp-wispy-moji-9349/web/lara-forum/artisan schedule:run
+/usr/local/php/7.3/bin/php /home/users/2/candypop.jp-wispy-moji-9349/web/lara-forum/artisan schedule:run >> /dev/null 2>&1
