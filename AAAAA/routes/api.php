c33a3caddaa2c2e8cf85f9a7af533b072a8b22c4
vagrant <vagrant@restfulapi>
2021-02-21 11:19:17 +0000

    create model & relationship

--- AAAAA/routes/api.php
+++ AAAAA/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Buyer\BuyerController;
 use Illuminate\Http\Request;
 
 /*
@@ -13,6 +14,18 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+
+Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+
+Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
