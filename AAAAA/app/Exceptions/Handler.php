4b07c87b96bd73ba350a90792c46a7aaa760dff5
vagrant <vagrant@restfulapi>
2021-04-18 05:40:20 +0000

    implement error handling with the laravel handler

--- AAAAA/app/Exceptions/Handler.php
+++ AAAAA/app/Exceptions/Handler.php
@@ -3,10 +3,20 @@
 namespace App\Exceptions;
 
 use Exception;
+use App\Traits\ApiResponser;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
     /**
      * A list of the exception types that are not reported.
      *
@@ -48,8 +58,81 @@ class Handler extends ExceptionHandler
      *
      * @throws \Exception
      */
+
+    //'render' method is executed every time that an exception is thrown.
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        //errorの型がvalidationExceptionの場合のみ適用
+        if ($exception instanceof ValidationException) {
+            return $this->convertValidationExceptionToResponse($exception, $request);
+        }
+        //modelが存在しないとき
+        if ($exception instanceof ModelNotFoundException) {
+            //model名も出力
+            $modelName = strtolower(class_basename($exception->getModel()));
+            return $this->errorResponse("Does not exists any {$modelName} with the specified identifier", 404);
+        }
+        //未認証ユーザ
+        if ($exception instanceof AuthenticationException) {
+            return $this->unauthenticated($request, $exception);
+        }
+        //未認可
+        if ($exception instanceof AuthorizationException) {
+            return $this->errorResponse($exception->getMessage(), 403);
+        }
+        //エンドポイントはあってるがmethodが違う
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse('The specified method for the requests is invalid', 405);
+        }
+        //undefined endpoint in route
+        if ($exception instanceof NotFoundHttpException) {
+            return $this->errorResponse('The specified URL cannot be found', 404);
+        }
+        //それ以外のhttp関連エラー
+        if ($exception instanceof HttpException) {
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+        //RDBなのでtransactionやproductとrelateされたuserは削除できない
+        if ($exception instanceof QueryException) {
+            // dd($exception);
+            $errorCode = $exception->errorInfo[1];
+            if ($errorCode == 1451) {
+                return $this->errorResponse('Cannot remove this resource permanently . It is related with any other resource', 409);
+            }
+        }
+        if (config('app.debug')) { //dev環境の場合はrendering
+            //laravelのエラーペーじをrenderingしているメソッドがこれ↓
+            return parent::render($request, $exception);
+        }
+
+        return $this->errorResponse('Unexpected Exception. Try later', 500);
+    }
+
+    /**
+     * Convert an authentication exception into an unauthenticated response.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Illuminate\Auth\AuthenticationException $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse('Unauthenticated.', 401);
+    }
+
+
+
+    /**
+     * Create a response object from the given validation exception.
+     *
+     * @param  \Illuminate\Validation\ValidationException  $e
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    //validationエラー時に自動的に404ページに飛ぶ仕様になっているため、API仕様にoverwriteする
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    {
+        $errors = $e->validator->errors()->getMessages();
+        return $this->errorResponse($errors, 422);
     }
 }
