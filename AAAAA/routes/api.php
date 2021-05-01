6b9812aab5614d943748f03535c20042cc367684
vagrant <vagrant@restfulapi>
2021-05-01 09:46:32 +0000

    implement operations for seller

--- AAAAA/routes/api.php
+++ AAAAA/routes/api.php
@@ -25,10 +25,18 @@ Route::resource('buyers.sellers', 'Buyer\BuyerSellerController', ['only' => ['in
 Route::resource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' => ['index']]);
 
 Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+Route::resource('categories.buyers', 'Category\CategoryBuyerController', ['only' => ['index']]);
+Route::resource('categories.sellers', 'Category\CategorySellerController', ['only' => ['index']]);
+Route::resource('categories.products', 'Category\CategoryProductController', ['only' => ['index']]);
+Route::resource('categories.transactions', 'Category\CategoryTransactionController', ['only' => ['index']]);
 
 Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
 
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+Route::resource('sellers.products', 'Seller\SellerProductController', ['except' => ['create', 'show', 'edit']]);
+Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only' => ['index']]);
+Route::resource('sellers.categories', 'Seller\SellerCategoryController', ['only' => ['index']]);
+Route::resource('sellers.buyers', 'Seller\SellerBuyerController', ['only' => ['index']]);
 
 Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
 
