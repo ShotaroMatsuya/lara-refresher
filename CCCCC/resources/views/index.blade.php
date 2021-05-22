7805a81e850fa65be5efd0844f2a0213fd1450a5
ShotaroMatsuya <matsuya@h01.itscom.net>
2021-05-22 21:34:16 +0900

    learning component in view template

---
+++ CCCCC/resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<x-layout>
+    <x-slot name="title">
+        {{-- slot以外の変数を埋め込みたい場合はlayoutで定義した変数をname属性に渡す --}}
+        My BBS
+    </x-slot>
+    {{-- lauyoutコンポーネントのslotに読み込ませたいcodeをここに書く --}}
+    <h1>My BBS</h1>
+
+    <ul>
+        {{-- @foreach ($posts as $post)
+            <li>{{ $post }}</li>
+        @endforeach --}}
+        @forelse ($posts as $index => $post)
+        <a href="{{ route('posts.show',$index) }}">
+            <li>{{ $post }}</li>
+        </a>
+        @empty
+            <li>No posts yet!</li>
+        @endforelse
+    </ul>
+</x-layout>
