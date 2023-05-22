87ff58bf67eefb4960e8cbe1a397084b110d6c24
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-05-22 15:39:17 +0900

    learn: making row query with db facade

--- app/routes/web.php
+++ app/routes/web.php
@@ -21,22 +21,23 @@ Route::get('/', function () {
     // $users = $pdo->query('select * from users')->fetchAll();
     
     // 2. DBファサードを使ってrowクエリを書く
-    // 2-1. selectクエリ
+    // 2-1. selectクエリ(配列が帰ってくる)
     // $result = DB::select('select * from users where id = ? and name = ?', [1, 'Adalberto Gerlach']);
     // $result = DB::select('select * from users where id = :id', ['id' => 1]);
     // 2-2. insertクエリ
     // DB::insert('insert into users (name, email,password) values (?, ?, ?)', ['Inserted Name', 'email@fdf.fd','passw']);
-    // 2-3. updateタイプ
+    // 2-3. updateタイプ(affectされたrecord数が帰ってくる)
     // $affected = DB::update('update users set email = "updatedemail@email.com" where email = ?', ['email@fdf.fd']);
-    // 2-4. deleteタイプ
+    // 2-4. deleteタイプ(deleteされたrecord数が帰ってくる)
     // $deleted = DB::delete('delete from users where id = ?',[4]);
     // 2-5. truncateタイプ
     // DB::statement('truncate table users');
     
-    // 3. DBファサードでクエリビルダーを使用
-    // $result = DB::select('select * from users');
-    // $result = DB::table('users')->select()->get();
-    $result = User::all();
+    // 3. DBファサード(rowクエリ版)とDBファサード(クエリビルダ版)で比較
+    // $result = DB::select('select * from users'); //arrayが入る
+    // $result = DB::table('users')->select()->get(); //collection型が入る
+    // Eloquentで書く
+    $result = User::all(); //eloquentのcollection型が入る(relationshipのあるデータを取扱さいに便利)
 
     dump($result);
 
