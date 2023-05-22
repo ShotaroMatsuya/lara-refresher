dd8b0cce2b4d13a1f1526c491f919381b2947e04
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-05-22 10:52:54 +0900

    chore: clean up old dir caches

--- D_forum/start_artisan.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /home/users/2/candypop.jp-wispy-moji-9349/web/lara-forum/
-/usr/local/php/7.3/bin/php artisan cache:clear
-/usr/local/php/7.3/bin/php artisan schedule:run >> /dev/null 2>&1
