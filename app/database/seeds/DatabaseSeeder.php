102ed56184349f1a4061f4ba122fa76b8eb21a6b
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-05-22 18:11:29 +0900

    chore:create and apply seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(CommentSeeder::class);
         $this->call(RoomSeeder::class);
+        $this->call(ReservationSeeder::class);
     }
 }
+
