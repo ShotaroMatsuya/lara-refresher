5c6d1845533d34da968dbbb449eceb2511c0a713
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-06-01 18:43:58 +0900

    feat: create hotel model

--- app/database/seeds/CountrySeeder.php
+++ app/database/seeds/CountrySeeder.php
@@ -11,10 +11,18 @@ class CountrySeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Country::class, 10)
+        $countries = factory(App\Country::class, 10)
             ->create()
             ->each(function ($country) {
                 $country->cities()->saveMany(factory(App\City::class, mt_rand(1, 10))->make());
             });
+        
+            foreach($countries as $country)
+            {
+                foreach ($country->cities as $city)
+                {
+                    $city->hotels()->saveMany(factory(App\Hotel::class, mt_rand(1, 4))->make());
+                }
+            }
     }
 }
