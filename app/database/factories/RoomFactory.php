3ba81f4d2ac3f6310206c1ecbd7163351a8d9f7b
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-06-02 11:18:57 +0900

    feat: create roomtype model

--- app/database/factories/RoomFactory.php
+++ app/database/factories/RoomFactory.php
@@ -10,6 +10,7 @@ $factory->define(Room::class, function (Faker $faker) {
         'name' => ucfirst($faker->text(20)),
         'description' => $faker->sentence(),
         'created_at' => $faker->dateTimeBetween('-10 days', '-5 days'),
-        'updated_at' => $faker->dateTimeBetween('-3 days', '-1 hour')
+        'updated_at' => $faker->dateTimeBetween('-3 days', '-1 hour'),
+        'room_type_id' => factory(App\RoomType::class)->create()
     ];
 });
