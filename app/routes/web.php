0c825f73e35041692e4d706204aabf4b4ebfc941
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-06-02 13:09:56 +0900

    learn: sort collection

--- app/routes/web.php
+++ app/routes/web.php
@@ -21,54 +21,54 @@ Route::get('/', function () {
     
     $city_id = 2;
 
-    // $result = DB::table('rooms')
-    // ->select('rooms.*', 'room_types.size', 'room_types.price', 'room_types.amount', 'hotels.name as hotel_name', 'hotels.id as hotel_id')
-    // ->join('room_types', 'rooms.room_type_id', '=', 'room_types.id')
-    // ->join('hotels', 'rooms.hotel_id', '=', 'hotels.id')
-    // ->whereNotExists(function ($query) use ($check_in, $check_out) {
-    //     $query->select('reservations.id')
-    //             ->from('reservations')
-    //             ->join('reservation_room', 'reservations.id', '=', 'reservation_room.reservation_id')
-    //             ->whereColumn('rooms.id', 'reservation_room.room_id')
-    //             ->where(function ($q) use ($check_in, $check_out) {
-    //                     $q->where('check_out', '>', $check_in);
-    //                     $q->where('check_in', '<', $check_out);
-    //                 })
-    //                 ->limit(1);
-    // })
-    // ->whereExists(function($q) use($city_id) {
-    //     $q->select('hotels.id')
-    //             ->from('hotels')
-    //             ->whereColumn('rooms.hotel_id','hotels.id')
-    //             ->whereExists(function($q) use($city_id) {
-    //                 $q->select('cities.id')
-    //                 ->from('cities')
-    //                 ->whereColumn('cities.id','hotels.city_id')
-    //                 ->where('id', $city_id)
-    //                 ->limit(1);
-    //             })
-    //             ->limit(1);
-    // })
-    // ->where('room_types.amount', '>', 0)
-    // ->paginate(10);
-    
-    $result = Room::with(['type', 'hotel'])
-        ->whereDoesntHave('reservations' , function($q) use ($check_in, $check_out) {
-                    $q->where(function($q) use($check_in, $check_out) {
+    $result = DB::table('rooms')
+    ->select('rooms.*', 'room_types.size', 'room_types.price', 'room_types.amount', 'hotels.name as hotel_name', 'hotels.id as hotel_id')
+    ->join('room_types', 'rooms.room_type_id', '=', 'room_types.id')
+    ->join('hotels', 'rooms.hotel_id', '=', 'hotels.id')
+    ->whereNotExists(function ($query) use ($check_in, $check_out) {
+        $query->select('reservations.id')
+                ->from('reservations')
+                ->join('reservation_room', 'reservations.id', '=', 'reservation_room.reservation_id')
+                ->whereColumn('rooms.id', 'reservation_room.room_id')
+                ->where(function ($q) use ($check_in, $check_out) {
                         $q->where('check_out', '>', $check_in);
                         $q->where('check_in', '<', $check_out);
-                });
-            })
-            ->whereHas('hotel.city', function($q) use ($city_id) {
-                $q->where('id', $city_id);
-            })
-            ->whereHas('type', function($q) {
-                $q->where('amount', '>', 0);
-            })
-            ->paginate(10);
+                    })
+                    ->limit(1);
+    })
+    ->whereExists(function($q) use($city_id) {
+        $q->select('hotels.id')
+                ->from('hotels')
+                ->whereColumn('rooms.hotel_id','hotels.id')
+                ->whereExists(function($q) use($city_id) {
+                    $q->select('cities.id')
+                    ->from('cities')
+                    ->whereColumn('cities.id','hotels.city_id')
+                    ->where('id', $city_id)
+                    ->limit(1);
+                })
+                ->limit(1);
+    })
+    ->where('room_types.amount', '>', 0)
+    ->orderBy('room_types.price', 'asc')
+    ->paginate(10);
+    
+    // $result = Room::with(['type', 'hotel'])
+    //     ->whereDoesntHave('reservations' , function($q) use ($check_in, $check_out) {
+    //                 $q->where(function($q) use($check_in, $check_out) {
+    //                     $q->where('check_out', '>', $check_in);
+    //                     $q->where('check_in', '<', $check_out);
+    //             });
+    //         })
+    //         ->whereHas('hotel.city', function($q) use ($city_id) {
+    //             $q->where('id', $city_id);
+    //         })
+    //         ->whereHas('type', function($q) {
+    //             $q->where('amount', '>', 0);
+    //         })
+    //         ->paginate(10)
+    //         ->sortBy('type.price'); // sortByDesc()
 
-    
-    
     dump($result);
     
     return view('welcome');
