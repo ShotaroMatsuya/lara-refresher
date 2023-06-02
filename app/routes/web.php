f2b9d28d7f81c1fd8a9055beeebe6541f24d096f
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-06-02 15:37:24 +0900

    feat: Order users by reservations

--- app/routes/web.php
+++ app/routes/web.php
@@ -18,22 +18,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $hotel_id = range(1,10);
-  
-    // $result = Hotel::whereIn('id',$hotel_id)
-    //         ->withCount('rooms')
-    //         ->orderBy('rooms_count', 'desc')
-    //         ->get();
-            
-    $result = DB::table('rooms')
-            ->join('room_types','rooms.room_type_id', '=', 'room_types.id')
-            ->selectRaw('sum(room_types.amount) as number_of_single_rooms, rooms.name')
-            ->groupBy('rooms.name', 'room_types.size')
-            ->having('room_types.size', '=', 1)
-            ->whereIn('rooms.hotel_id', $hotel_id)
-            ->orderBy('number_of_single_rooms', 'desc')
-            ->get();
- 
+    $result = DB::table('users')->orderByDesc(
+        DB::table('reservations')
+            ->select('price')
+            ->whereColumn('users.id', 'reservations.user_id')
+            ->orderByDesc('price')
+            ->limit(1)
+    )->get();
+    
     dump($result);
     
     return view('welcome');
