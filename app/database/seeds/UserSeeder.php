15370abeaf6995be4374033fe29c843ca8b318a8
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-06-02 11:48:39 +0900

    feat: create pivot table for reservation and room

--- app/database/seeds/UserSeeder.php
+++ app/database/seeds/UserSeeder.php
@@ -12,7 +12,22 @@ class UserSeeder extends Seeder
     public function run()
     {
         factory(App\User::class, 10)
-            ->create();
+            ->create()
+            ->each(function ($user) {
+                $reservations = $user->reservations()->saveMany(factory(App\Reservation::class, mt_rand(1, 3))->make());
+
+            foreach($reservations as $reservation) 
+            {
+                $room_ids = [];
+                for($i=1; $i<= mt_rand(1, 3); $i++)
+                {
+                    array_push($room_ids, mt_rand(1, 20));
+                }
+                $reservation->rooms()->attach($room_ids, ['status' => (bool)random_int(0,1)]);
+            }
+        });
+
+
     }
 }
 
